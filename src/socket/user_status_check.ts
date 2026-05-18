5d45dc4795f93224ee215524f5eb698df532fa35
Daksh Mahajan <122230976+daksh0110@users.noreply.github.com>
2026-05-19 00:25:09 +0530

    fix group chat user status check

--- src/socket/user_status_check.ts
+++ src/socket/user_status_check.ts
@@ -1,5 +1,6 @@
 import { Socket } from "socket.io";
 import { onlineUsers } from "./socket.connection";
+import { ChatMemberModel } from "../models/chat_group_member.modal";
 
 export const checkUserStatus = (socket: Socket) => {
   socket.on("check_user_status", ({ userId }) => {
@@ -10,4 +11,25 @@ export const checkUserStatus = (socket: Socket) => {
       online: isOnline,
     });
   });
+
+  socket.on("check_group_status", async ({ chatId }) => {
+    try {
+      const members = await ChatMemberModel.find({
+        chat_id: chatId,
+        is_active: true,
+      }).select("user_id");
+
+      const onlineMembers = members
+        .map((m) => m.user_id.toString())
+        .filter((id) => onlineUsers.has(id));
+
+      socket.emit("group_status", {
+        chatId,
+        onlineMembers,
+      });
+    } catch (error) {
+      console.error("check_group_status error:", error);
+    }
+  });
 };
+
